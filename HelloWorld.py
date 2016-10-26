858577e351aaf2b3576311510aae37b921166f5c
Tony Calcaterra <tcalcaterra@PC01.cis.com>
2016-10-26 10:13:04 -0500

    added HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+# HelloWorld.py
+# Tony Calcaterra
+# 10/26/2016
+
+""" Program that thells the world hello """
+
+print('Hello World!')
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Hello_World
+just a test
+
+CHANGELOG
+added Hello_world.py - 10/26/2016
